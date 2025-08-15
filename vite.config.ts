a5312be7c0c22d8f4ed73ef614720b644a008cda
Kher Phay Chang <kayphay92@gmail.com>
2025-08-15 23:03:20 +0900

    add base

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,10 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { intlayerPlugin } from "vite-intlayer";
+import pkg from "./package.json";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), intlayerPlugin()],
+  base: `/${pkg.name}/`,
 });
